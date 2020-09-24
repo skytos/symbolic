e88a35761a47a798bcaa595f2cfd0849557dd1cc
Skylar Broad <skylar.broad@gmail.com>
2020-09-24 16:48:41 -0700

    lowercase everything to fix lint :P

--- symbolic.go
+++ symbolic.go
@@ -5,193 +5,192 @@ import (
 	"math"
 )
 
-type Expression interface {
-	String() string
-	Derivative(v Variable) Expression
-	Evaluate(vals map[string]float64) float64
+type expression interface {
+	string() string
+	derivative(v variable) expression
+	evaluate(vals map[string]float64) float64
 }
 
-type Constant struct {
+type constant struct {
 	value float64
 }
 
-type Variable struct {
+type variable struct {
 	name string
 }
 
-type Sum struct {
-	a, b Expression
+type sum struct {
+	a, b expression
 }
 
-type Product struct {
-	a, b Expression
+type product struct {
+	a, b expression
 }
 
-type Power struct {
-	base, exponent Expression
+type power struct {
+	base, exponent expression
 }
 
-type Sin struct {
-	value Expression
+type sin struct {
+	value expression
 }
 
-type Cos struct {
-	value Expression
+type cos struct {
+	value expression
 }
 
-var ONE = Constant{1}
-var NEGATIVE_ONE = Constant{-1}
-var ZERO = Constant{0}
+var one = constant{1}
+var negativeOne = constant{-1}
+var zero = constant{0}
 
-func (self Constant) String() string {
-	return fmt.Sprintf("%v", self.value)
+func (e constant) string() string {
+	return fmt.Sprintf("%v", e.value)
 }
 
-func (self Constant) Derivative(v Variable) Expression {
-	return ZERO
+func (e constant) derivative(v variable) expression {
+	return zero
 }
 
-func (self Constant) Evaluate(_vals map[string]float64) float64 {
-	return self.value
+func (e constant) evaluate(_vals map[string]float64) float64 {
+	return e.value
 }
 
-func (self Variable) String() string {
-	return self.name
+func (e variable) string() string {
+	return e.name
 }
 
-func (self Variable) Derivative(v Variable) Expression {
-	if v.name == self.name {
-		return ONE
-	} else {
-		return ZERO
+func (e variable) derivative(v variable) expression {
+	if v.name == e.name {
+		return one
+	}
+	return zero
+}
+
+func (e variable) evaluate(vals map[string]float64) float64 {
+	return vals[e.name]
+}
+
+func (e sum) string() string {
+	return fmt.Sprintf("(%v+%v)", e.a, e.b)
+}
+
+func (e sum) derivative(v variable) expression {
+	return sum{e.a.derivative(v), e.b.derivative(v)}
+}
+
+func (e sum) evaluate(vals map[string]float64) float64 {
+	return e.a.evaluate(vals) + e.b.evaluate(vals)
+}
+
+func (e product) string() string {
+	return fmt.Sprintf("(%v*%v)", e.a, e.b)
+}
+
+func (e product) derivative(v variable) expression {
+	return sum{
+		product{e.a.derivative(v), e.b},
+		product{e.a, e.b.derivative(v)},
 	}
 }
 
-func (self Variable) Evaluate(vals map[string]float64) float64 {
-	return vals[self.name]
+func (e product) evaluate(vals map[string]float64) float64 {
+	return e.a.evaluate(vals) * e.b.evaluate(vals)
 }
 
-func (self Sum) String() string {
-	return fmt.Sprintf("(%v+%v)", self.a, self.b)
+func (e power) string() string {
+	return fmt.Sprintf("%v^%v", e.base, e.exponent)
 }
 
-func (self Sum) Derivative(v Variable) Expression {
-	return Sum{self.a.Derivative(v), self.b.Derivative(v)}
+func (e power) derivative(v variable) expression {
+	return product{
+		e.exponent,
+		product{e.base.derivative(v),
+			power{e.base,
+				sum{e.exponent, negativeOne}}}}
 }
 
-func (self Sum) Evaluate(vals map[string]float64) float64 {
-	return self.a.Evaluate(vals) + self.b.Evaluate(vals)
+func (e power) evaluate(vals map[string]float64) float64 {
+	return math.Pow(e.base.evaluate(vals), e.exponent.evaluate(vals))
 }
 
-func (self Product) String() string {
-	return fmt.Sprintf("(%v*%v)", self.a, self.b)
+func (e sin) string() string {
+	return fmt.Sprintf("sin(%v)", e.value)
 }
 
-func (self Product) Derivative(v Variable) Expression {
-	return Sum{
-		Product{self.a.Derivative(v), self.b},
-		Product{self.a, self.b.Derivative(v)},
-	}
+func (e sin) derivative(v variable) expression {
+	return product{e.value.derivative(v), cos{e.value}}
 }
 
-func (self Product) Evaluate(vals map[string]float64) float64 {
-	return self.a.Evaluate(vals) * self.b.Evaluate(vals)
+func (e sin) evaluate(vals map[string]float64) float64 {
+	return math.Sin(e.value.evaluate(vals))
+}
+func (e cos) string() string {
+	return fmt.Sprintf("cos(%v)", e.value)
 }
 
-func (self Power) String() string {
-	return fmt.Sprintf("%v^%v", self.base, self.exponent)
+func (e cos) derivative(v variable) expression {
+	return product{negate(e.value.derivative(v)), sin{e.value}}
 }
 
-func (self Power) Derivative(v Variable) Expression {
-	return Product{
-		self.exponent,
-		Product{self.base.Derivative(v),
-			Power{self.base,
-				Sum{self.exponent, NEGATIVE_ONE}}}}
+func (e cos) evaluate(vals map[string]float64) float64 {
+	return math.Cos(e.value.evaluate(vals))
 }
 
-func (self Power) Evaluate(vals map[string]float64) float64 {
-	return math.Pow(self.base.Evaluate(vals), self.exponent.Evaluate(vals))
+func negate(e expression) expression {
+	return product{e, negativeOne}
 }
 
-func (self Sin) String() string {
-	return fmt.Sprintf("sin(%v)", self.value)
+func invert(e expression) expression {
+	return power{e, negativeOne}
 }
 
-func (self Sin) Derivative(v Variable) Expression {
-	return Product{self.value.Derivative(v), Cos{self.value}}
+func euler(e expression, v variable) expression {
+	return sum{v, negate(product{e, invert(e.derivative(v))})}
 }
 
-func (self Sin) Evaluate(vals map[string]float64) float64 {
-	return math.Sin(self.value.Evaluate(vals))
-}
-func (self Cos) String() string {
-	return fmt.Sprintf("cos(%v)", self.value)
-}
-
-func (self Cos) Derivative(v Variable) Expression {
-	return Product{negate(self.value.Derivative(v)), Sin{self.value}}
-}
-
-func (self Cos) Evaluate(vals map[string]float64) float64 {
-	return math.Cos(self.value.Evaluate(vals))
-}
-
-func negate(e Expression) Expression {
-	return Product{e, NEGATIVE_ONE}
-}
-
-func invert(e Expression) Expression {
-	return Power{e, NEGATIVE_ONE}
-}
-
-func euler(e Expression, v Variable) Expression {
-	return Sum{v, negate(Product{e, invert(e.Derivative(v))})}
-}
-
-func quadratic(a, b, c Expression) Expression {
-	return Product{
-		Sum{
+func quadratic(a, b, c expression) expression {
+	return product{
+		sum{
 			negate(b),
-			negate(Power{
-				Sum{
-					Power{b, Constant{2}},
-					Product{
-						Constant{-4},
-						Product{a, c},
+			negate(power{
+				sum{
+					power{b, constant{2}},
+					product{
+						constant{-4},
+						product{a, c},
 					},
 				},
-				Constant{0.5},
+				constant{0.5},
 			}),
 		},
-		invert(Product{Constant{2}, a}),
+		invert(product{constant{2}, a}),
 	}
 }
 
 func main() {
-	h := Variable{"h"}
-	s := Variable{"s"}
-	a := Variable{"a"}
+	h := variable{"h"}
+	s := variable{"s"}
+	a := variable{"a"}
 
 	time := quadratic(
-		Constant{-9.8},
-		Product{s, Sin{a}},
+		constant{-9.8},
+		product{s, sin{a}},
 		h,
 	)
 
-	distance := Product{
+	distance := product{
 		s,
-		Product{
-			Cos{a},
+		product{
+			cos{a},
 			time,
 		},
 	}
 
-	derivativeOfDistance := distance.Derivative(a)
+	derivativeofdistance := distance.derivative(a)
 
 	for i, z := 0, 0.5; i < 10; i++ {
 		fmt.Println(z)
-		z = euler(derivativeOfDistance, a).Evaluate(
+		z = euler(derivativeofdistance, a).evaluate(
 			map[string]float64{"h": 1.5, "s": 1.0, "a": z},
 		)
 	}
