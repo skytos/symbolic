341749ec6dda89bff5560f8e4cbfd5f2e5921cf0
Skylar Broad <skylar.broad@gmail.com>
2021-04-14 01:42:22 -0700

    add simplification code

--- symbolic.go
+++ symbolic.go
@@ -9,6 +9,7 @@ type expression interface {
 	String() string
 	derivative(v variable) expression
 	evaluate(vals map[string]float64) float64
+	simplify() expression
 }
 
 type constant struct {
@@ -31,14 +32,6 @@ type power struct {
 	base, exponent expression
 }
 
-type sin struct {
-	value expression
-}
-
-type cos struct {
-	value expression
-}
-
 var one = constant{1}
 var negativeOne = constant{-1}
 var zero = constant{0}
@@ -53,6 +46,10 @@ func (e constant) derivative(v variable) expression {
 
 func (e constant) evaluate(_vals map[string]float64) float64 {
 	return e.value
+}
+
+func (e constant) simplify() expression {
+	return e
 }
 
 func (e variable) String() string {
@@ -70,6 +67,10 @@ func (e variable) evaluate(vals map[string]float64) float64 {
 	return vals[e.name]
 }
 
+func (e variable) simplify() expression{
+	return e
+}
+
 func (e sum) String() string {
 	return fmt.Sprintf("(%v + %v)", e.a, e.b)
 }
@@ -80,6 +81,28 @@ func (e sum) derivative(v variable) expression {
 
 func (e sum) evaluate(vals map[string]float64) float64 {
 	return e.a.evaluate(vals) + e.b.evaluate(vals)
+}
+
+var ZERO = constant{0.0}
+var ONE = constant{1.0}
+
+func (e sum) simplify() expression {
+	a := e.a.simplify()
+	b := e.b.simplify()
+
+	if a == ZERO {
+		return b
+	} else if b == ZERO {
+		return a
+	} else {
+		ac, a_ok := a.(constant)
+		bc, b_ok := b.(constant)
+		if a_ok && b_ok {
+			return constant{ac.value + bc.value}
+		} else {
+			return sum{a, b}
+		}
+	}
 }
 
 func (e product) String() string {
@@ -95,6 +118,29 @@ func (e product) derivative(v variable) expression {
 
 func (e product) evaluate(vals map[string]float64) float64 {
 	return e.a.evaluate(vals) * e.b.evaluate(vals)
+}
+
+func (e product) simplify() expression {
+	a := e.a.simplify()
+	b := e.b.simplify()
+
+	if a == ZERO {
+		return a
+	} else if b == ZERO {
+		return b
+	} else if a == ONE {
+		return b
+	} else if b == ONE {
+		return a
+	} else {
+		ac, a_ok := a.(constant)
+		bc, b_ok := b.(constant)
+		if a_ok && b_ok {
+			return constant{ac.value * bc.value}
+		} else {
+			return product{a, b}
+		}
+	}
 }
 
 func (e power) String() string {
@@ -113,28 +159,25 @@ func (e power) evaluate(vals map[string]float64) float64 {
 	return math.Pow(e.base.evaluate(vals), e.exponent.evaluate(vals))
 }
 
-func (e sin) String() string {
-	return fmt.Sprintf("sin(%v)", e.value)
-}
+func (e power) simplify() expression {
+	base := e.base.simplify()
+	exponent := e.exponent.simplify()
 
-func (e sin) derivative(v variable) expression {
-	return product{e.value.derivative(v), cos{e.value}}
-}
-
-func (e sin) evaluate(vals map[string]float64) float64 {
-	return math.Sin(e.value.evaluate(vals))
-}
-
-func (e cos) String() string {
-	return fmt.Sprintf("cos(%v)", e.value)
-}
-
-func (e cos) derivative(v variable) expression {
-	return product{negate(e.value.derivative(v)), sin{e.value}}
-}
-
-func (e cos) evaluate(vals map[string]float64) float64 {
-	return math.Cos(e.value.evaluate(vals))
+	if exponent == ZERO {
+		return ONE
+	} else if exponent == ONE {
+		return base
+	} else if base == ZERO || base == ONE{
+		return base
+	} else {
+		basec, base_ok := base.(constant)
+		exponentc, exponent_ok := exponent.(constant)
+		if base_ok && exponent_ok {
+			return constant{math.Pow(basec.value, exponentc.value)}
+		} else {
+			return power{base, exponent}
+		}
+	}
 }
 
 func negate(e expression) expression {
@@ -165,29 +208,6 @@ func pow(e1, e2 expression) expression {
 	return power{e1,e2}
 }
 
-func euler(e expression, v variable) expression {
-	return sum{v, negate(product{e, invert(e.derivative(v))})}
-}
-
-func quadratic(a, b, c expression) expression {
-	return product{
-		sum{
-			negate(b),
-			negate(power{
-				sum{
-					power{b, constant{2}},
-					product{
-						constant{-4},
-						product{a, c},
-					},
-				},
-				constant{0.5},
-			}),
-		},
-		invert(product{constant{2}, a}),
-	}
-}
-
 func main() {
 	two := constant{2.0}
 	three := constant{3.0}
@@ -214,23 +234,24 @@ func main() {
 	eb := pow(sub(three, b), two)
 	ec := pow(sub(two, c), two)
 
-	e := add(ea, add(eb, ec))
+	e := add(ea, add(eb, ec)).simplify()
 
-	gm1 := e.derivative(m1)
-	gm2 := e.derivative(m2)
-	gm3 := e.derivative(m3)
-	gm4 := e.derivative(m4)
-	gb3 := e.derivative(b3)
+	gm1 := e.derivative(m1).simplify()
+	gm2 := e.derivative(m2).simplify()
+	gm3 := e.derivative(m3).simplify()
+	gm4 := e.derivative(m4).simplify()
+	gb3 := e.derivative(b3).simplify()
 
 	vars := map[string]float64{
-		"m1": 0.5,
-		"m2": 0.2,
-		"m3": -0.27,
-		"m4": -0.8,
-		"b3": 2.24,
+		"m1": 1.0,
+		"m2": 0.5,
+		"m3": -0.5,
+		"m4": -1.0,
+		"b3": 2.0,
 	}
-	for i := 0; i < 100000; i++ {
-		delta := 0.0001
+	for i := 0; i < 15000; i++ {
+		// fmt.Println(math.Log(e.evaluate(vars)))
+		delta := 0.0007
 		dm1 := -delta * gm1.evaluate(vars)
 		dm2 := -delta * gm2.evaluate(vars)
 		dm3 := -delta * gm3.evaluate(vars)
